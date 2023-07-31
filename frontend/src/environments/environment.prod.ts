7e8fed0a0283040fd87419e483b4ba7e6a37e1c7
Laurence Scotford <laurencescotford@gmail.com>
2023-07-31 18:06:09 +0000

    Add deployment step for front end

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  version: "0.0.2",
+  version: "0.0.3",
   production: true,
   apiURL: 'http://Udacity-fsjd-project-4-env.eba-ktyeqd9m.us-east-1.elasticbeanstalk.com:3000/'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  version: "0.0.2",
+  version: "0.0.3",
   production: false,
   apiURL: 'http://localhost:3000/'
 };
